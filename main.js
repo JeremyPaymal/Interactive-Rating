0abbd7f6e359ab8d6b667d38744f74cc7edced13
Jeremy Paymal <jeremy.paymal@gmail.com>
2022-12-06 18:00:13 -0500

    Main js modification

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 let selection;
 
-function myStar(number){
-    selection = number;
-    document.querySelectorAll('.rating_btn').forEach(element => element.classList.remove('selected'))
-    document.getElementById(number).classList.add('selected')
-    console.log(number)
+function myStar(number) {
+  selection = number;
+  document
+    .querySelectorAll(".rating_btn")
+    .forEach((element) => element.classList.remove("selected"));
+  document.getElementById(number).classList.add("selected");
+  console.log(number);
 }
 
-function submission(){
-    document.getElementById('rating_state').classList.add('hidden')
-    document.getElementById('chosen_rating').innerText = selection
-    document.getElementById('thanks').classList.remove('hidden')
+function submission() {
+  if (selection.isNaN) {
+  } else {
+    document.getElementById("rating_state").classList.add("hidden");
+    document.getElementById("chosen_rating").innerText = selection;
+    document.getElementById("thanks").classList.remove("hidden");
+  }
 }
